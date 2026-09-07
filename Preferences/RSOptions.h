746e7a98dc363d3b4258fae6c0302f608bc69767
moxuan1121 <99877708+moxuan1121@users.noreply.github.com>
2026-09-07 10:09:06 +0900

    Declare shared option functions with C linkage for Logos

--- Preferences/RSOptions.h
+++ Preferences/RSOptions.h
@@ -1,7 +1,13 @@
 #import <Foundation/Foundation.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 NSArray<NSDictionary *> *RSOptionGroups(void);
 id RSValidateOption(NSDictionary *option, id value);
 id RSOption(NSString *key);
 void RSSetOption(NSString *key, id value);
 void RSReloadOptions(void);
+#ifdef __cplusplus
+}
+#endif
