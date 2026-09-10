054ef64536a613812f7031135e4846fa283eadf1
moxuan1121 <99877708+moxuan1121@users.noreply.github.com>
2026-09-10 12:46:52 +0900

    Export popup bridge to Logos hooks

--- KeyboardAI/RSKAInterface.h
+++ KeyboardAI/RSKAInterface.h
@@ -1,4 +1,7 @@
 #import <UIKit/UIKit.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 void RSKASelectionFeedback(void);
 BOOL RSKABeginAnswer(NSString *name, dispatch_block_t closed);
 void RSKAUpdateAnswer(NSString *text, BOOL finished, NSString *error);
@@ -6,3 +9,6 @@ void RSKAOpenTokens(NSString *text);
 
 void RSKAClosePanel(void);
 BOOL RSKAIsPanelVisible(void);
+#ifdef __cplusplus
+}
+#endif
